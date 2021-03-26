d3353461f5d307e71053f347a6b4c470849115eb
Michael Burton <micbur1@gmail.com>
2021-03-26 14:59:18 -0700

    resize all images

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,16 @@ $(document).ready(function() {
     console.log(quest5Ans);
 
     if (quest5Ans===1){
+      $(".main").hide();
       $(".jsresult").show();
     }else if (quest5Ans===2){
+      $(".main").hide();
       $(".csharpresult").show();
     }else if (quest5Ans===3){
+      $(".main").hide();
       $(".rubyresult").show();
     }else {
+      $(".main").hide();
       $(".confusedresult").show();
     }
   });
