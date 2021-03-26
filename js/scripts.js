ad359ccfa505217772ff5412f48ee04005b248e6
Michael Burton <micbur1@gmail.com>
2021-03-26 16:40:36 -0700

    move mainimg out of the container div and add hide and show function for mainimg in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -8,36 +8,44 @@ $(document).ready(function() {
 
     if (quest5Ans===1){
       $(".main").hide();
+      $(".mainimg").hide();
       $(".jsresult").show();
     }else if (quest5Ans===2){
       $(".main").hide();
+      $(".mainimg").hide();
       $(".csharpresult").show();
     }else if (quest5Ans===3){
       $(".main").hide();
+      $(".mainimg").hide();
       $(".rubyresult").show();
     }else {
       $(".main").hide();
+      $(".mainimg").hide();
       $(".confusedresult").show();
     }
   });
 
   $("#again1").click(function(){
     $(".jsresult").hide();
+    $(".mainimg").show();
     $(".main").show();
     document.getElementById("formOne").reset();
   });
   $("#again2").click(function(){
     $(".csharpresult").hide();
+    $(".mainimg").show();
     $(".main").show();
     document.getElementById("formOne").reset();
   });
   $("#again3").click(function(){
     $(".rubyresult").hide();
+    $(".mainimg").show();
     $(".main").show();
     document.getElementById("formOne").reset();
   });
   $("#again4").click(function(){
     $(".confusedresult").hide();
+    $(".mainimg").show();
     $(".main").show();
     document.getElementById("formOne").reset();
   });
